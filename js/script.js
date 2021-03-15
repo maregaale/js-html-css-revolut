590cd1852bb23381483d469bf14d96d97329647c
Alessandro Marega <ale_marega@hotmail.it>
2021-03-15 16:02:18 +0100

    aggiunto in js effetto apparizione/sparizione dei menu all'hover sui link

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+// variabili
+var mainNavLinks = $(".main-nav > ul > li");
+var linksMenu = $(".links-menu")
+// all'hover sui link della main-nav faccio sparire o comparire il menu
 
+mainNavLinks.mouseenter(
+  function () {
+    // linksMenu.fadeOut(50);
+    $(".main-nav > ul > li").children(".links-menu").hide();
+    $(this).children(".links-menu").show();
+  }
+);
+
+// mainNavLinks.mouseleave(
+//   function () {
+//     $(this).children(".links-menu").fadeOut(50);
+//   }
+// );
