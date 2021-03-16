2b88046f22c098741678787874aa5f431839177e
Alessandro Marega <ale_marega@hotmail.it>
2021-03-16 10:37:15 +0100

    aggiunto document-ready a script.js

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
-// variabili
-var mainNavLinks = $(".main-nav > ul > li");
-var linksMenu = $(".links-menu");
+$(function () {
 
-// all'hover sui link della main-nav faccio sparire o comparire il menu
-mainNavLinks.mouseenter(
-  function () {
-    $(".main-nav > ul > li").children(".links-menu").hide();
-    $(this).children(".links-menu").show();
-  }
-);
+  // variabili
+  var mainNavLinks = $(".main-nav > ul > li");
+  var linksMenu = $(".links-menu");
 
-// al click sui link della main-nav faccio sparire o comparire il menu
-mainNavLinks.click(
-  function () {
-    $(this).children(".links-menu").fadeToggle();
-  }
-);
-
-// al click sul documento si chiudono i menu
-$(document).click(
-  function (clickEvent) {
-    if(mainNavLinks != clickEvent.target && mainNavLinks.has(clickEvent.target).length === 0){
-      linksMenu.fadeOut();
+  // all'hover sui link della main-nav faccio sparire o comparire il menu
+  mainNavLinks.mouseenter(
+    function () {
+      $(".main-nav > ul > li").children(".links-menu").hide();
+      $(this).children(".links-menu").show();
     }
-  }
-);
+  );
+
+  // al click sui link della main-nav faccio sparire o comparire il menu
+  mainNavLinks.click(
+    function () {
+      $(this).children(".links-menu").fadeToggle();
+    }
+  );
+
+  // al click sul documento si chiudono i menu
+  $(document).click(
+    function (clickEvent) {
+      if(mainNavLinks != clickEvent.target && mainNavLinks.has(clickEvent.target).length === 0){
+        linksMenu.fadeOut();
+      }
+    }
+  );
+  
+});
